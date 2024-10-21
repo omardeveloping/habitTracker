d9e0a6e43c6a02260ca84c6e77f7e691a3c24031
Omar <omar.montanares@inacapmail.cl>
2024-10-21 19:09:35 -0300

    Funciones basicas e importes

--- habitTracker/habitTracker/urls.py
+++ habitTracker/habitTracker/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from habits import views as habits
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', habits.index),
 ]
--- habitTracker/habits/views.py
+++ habitTracker/habits/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from datetime import datetime
+from calendar import monthrange
 
 # Create your views here.
+
+def index(request):
+    current_date = datetime.now()
+    date = current_date.date()
+    letras = []
+
+    days = monthrange(current_date.year, current_date.month)
+    for i in range(1,days[1]):
+        letras.append("a")
+
+    data = {
+        "date": date,
+        "days": days[1],
+        "letras": letras,
+    }
+    return render(request, "habits.html", data)
